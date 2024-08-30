1272516e98d3ff5938af06be255a1eb2791d970d
Esteban <davidemg903@gmail.com>
2024-08-30 13:59:11 -0500

    formulario1

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormularioComponent } from './formulario/formulario.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormularioComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'notaria-fomeque';
+}
